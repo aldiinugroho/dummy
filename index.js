e867f6cb6bd41b6f120a9de4b154a13b4ca59dd6
aldi nugroho <aldinugroho@aldis-iMac.local>
2021-06-01 16:18:11 +0700

    updated

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3030
 
-const jsondata = require('./test.json')
+const jsondata = require('./model_bangli.json')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 app.get('/v1/api/bali/weather', (req, res) => {
   res.json(jsondata)
+})
+
+app.post('/v1/data', (req, res) => {
+  try {
+    console.log(req.body.name)
+    res.send(req.body.name)
+  } catch (error) {
+    res.json("faile")
+  }
 })
 
 app.listen(port, () => {
